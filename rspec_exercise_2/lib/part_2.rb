733d5103dd7edefa46398c63480354b918c5fe31
Santyru <86131872+Santyru-C@users.noreply.github.com>
2021-11-03 20:38:47 -0300

    Add files via upload

--- rspec_exercise_2/lib/part_2.rb
+++ rspec_exercise_2/lib/part_2.rb
@@ -1,9 +1,60 @@
 def palindrome?(str)
 
     str.each_char.with_index do |char, ind|
-        if char != str[str.length - ind]
+        if char != str[(-1 - ind)]
             return false
         end
     end
     return true
+end
+
+def substrings(str) # with while loop
+    arr = []
+    i = 0
+
+    while i < str.length
+        j = 0
+        
+        while j < str.length
+            if j >= i and !arr.include?(str[i..j])
+                arr << str[i..j]               
+            end
+            j += 1
+        end
+        i += 1
+    end
+    arr
+end
+
+# or
+
+# def substrings(str) # with each loop
+#     arr = []
+#     i = 0
+
+#     (i...str.length).each do
+#         j = 0
+        
+#         (j...str.length).each do
+#             if j >= i and !arr.include?(str[i..j])
+#                 p i, j
+#                 arr << str[i..j]
+                
+#             end
+#             j += 1
+#         end
+#         i += 1
+#     end
+#     arr
+# end
+
+def palindrome_substrings(str)
+    arr = []
+
+    substrings(str).each do |elem|
+        if elem.length > 1 and palindrome?(elem)
+            arr << elem
+        end
+    end
+    arr
 end
