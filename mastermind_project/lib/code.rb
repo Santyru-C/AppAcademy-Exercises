f44694ff059ff394f9ae96cbd65f76fd63195951
Santyru <86131872+Santyru-C@users.noreply.github.com>
2021-11-18 21:19:42 -0300

    Add files via upload

--- mastermind_project/lib/code.rb
+++ mastermind_project/lib/code.rb
@@ -8,16 +8,13 @@ class Code
   attr_reader :pegs
 
   def self.valid_pegs?(chars)
-    chars.each do |char|
-      return false if !POSSIBLE_PEGS.has_key?(char.upcase)
-    end
-    return true
+    chars.all? {|char| POSSIBLE_PEGS.has_key?(char.upcase)}
   end
 
   def initialize(pegs)
 
     raise "Not a valid PEG" if !Code.valid_pegs?(pegs)
-    @pegs = pegs.each(&:upcase!)
+    @pegs = pegs.map(&:upcase)
 
   end
 
@@ -27,6 +24,37 @@ class Code
     length.times {random_pegs << POSSIBLE_PEGS.keys.sample}
     Code.new(random_pegs)
   end
+
+  def self.from_string(pegs)
+    Code.new(pegs.split(""))
+  end
+
+  def [](index)
+    @pegs[index]
+  end
+
+  def length
+    @pegs.size
+  end
   
+  def num_exact_matches(guess)
+    count = 0
+    
+    @pegs.each_with_index do |peg, ind|
+      count += 1 if peg == guess[ind]
+    end
+    count
+  end
+
+  def num_near_matches(guess)
+    count = 0
+
+    (0...guess.pegs.size).each do |i|
+     count += 1 if guess[i] != @pegs[i] and @pegs.include?(guess[i])
+    end
+    # work in progress
+    count
+    
+  end
 
 end
--- mastermind_project/lib/test.rb
+++ mastermind_project/lib/test.rb
@@ -1,4 +1,6 @@
-ARR = [1,2,3,4]
+ARR = ["a","b","c","d"]
 other_arr = []
 other_arr << ARR.sample
+puts other_arr[0].upcase!
 puts ARR
+puts other_arr
