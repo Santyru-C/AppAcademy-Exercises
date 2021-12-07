0926820908779ec08d1bd424ba662814d260d4c8
Santyru <86131872+Santyru-C@users.noreply.github.com>
2021-12-07 19:21:36 -0300

    Add files via upload

--- recap_exercise_3/recap_3.rb
+++ recap_exercise_3/recap_3.rb
@@ -202,3 +202,61 @@ end
 # end
 # p word_2        # => "DiJkStRa"
 
+# Recursion problems
+
+def multiply(a, b)
+    return 0 if a == 0 or b == 0
+
+    if b >= 0
+        a + multiply(a, b - 1)
+    else
+        -(a + multiply(a, -b - 1))
+    end
+end
+
+# Examples
+# p multiply(3, 5)        # => 15
+# p multiply(5, 3)        # => 15
+# p multiply(2, 4)        # => 8
+# p multiply(0, 10)       # => 0
+# p multiply(-3, -6)      # => 18
+# p multiply(3, -6)       # => -18
+# p multiply(-3, 6)       # => -18
+
+def lucas_sequence(length)
+    return [] if length == 0
+    return [2] if length == 1
+    return [2, 1] if length == 2
+
+    seq = lucas_sequence(length - 1)
+    next_el = seq[-1] + seq[-2]
+    seq << next_el
+    seq
+end
+
+# Examples
+# p lucas_sequence(0)   # => []
+# p lucas_sequence(1)   # => [2]    
+# p lucas_sequence(2)   # => [2, 1]
+# p lucas_sequence(3)   # => [2, 1, 3]
+# p lucas_sequence(6)   # => [2, 1, 3, 4, 7, 11]
+# p lucas_sequence(8)   # => [2, 1, 3, 4, 7, 11, 18, 29]
+
+def prime_factorization(num)
+    (2...num).each do |fact|
+        if num % fact == 0
+            other_fact = num / fact
+            return [*prime_factorization(fact), *prime_factorization(other_fact)]
+        end
+    end
+    [ num ]
+end
+
+# Examples
+p prime_factorization(12)     # => [2, 2, 3]
+p prime_factorization(24)     # => [2, 2, 2, 3]
+p prime_factorization(25)     # => [5, 5]
+p prime_factorization(60)     # => [2, 2, 3, 5]
+p prime_factorization(7)      # => [7]
+p prime_factorization(11)     # => [11]
+p prime_factorization(2017)   # => [2017]
