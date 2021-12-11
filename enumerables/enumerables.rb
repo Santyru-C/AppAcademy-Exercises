c9c5f9f5edcdf5b3511a80dd0aec9981b1458513
Santyru <86131872+Santyru-C@users.noreply.github.com>
2021-12-11 18:34:15 -0300

    Add files via upload

--- enumerables/enumerables.rb
+++ enumerables/enumerables.rb
@@ -1,4 +1,35 @@
 class Array
     def my_each(&prc)
-        
+       self.each {|element| prc.call(element)}
     end
+
+
+
+    def my_select(&prc)
+        selection = []
+        
+        self.my_each {|element| selection << element if prc.call(element)}
+        selection
+    end
+
+    def my_reject(&prc)
+        not_rejected = []
+
+        self.my_each{|element| not_rejected << element if !prc.call(element)}
+        not_rejected
+    end
+
+    def my_any?(&prc)
+        self.my_each{|element| return true if prc.call(element)}
+        false
+    end
+
+    def my_all?(&prc)
+        self.my_each{|element| return false if !prc.call(element)}
+        true
+    end
+end
+
+a = [1, 2, 3]
+p a.my_all? { |num| num > 1 } # => false
+p a.my_all? { |num| num < 4 } # => true
